d696f11a605d0f825e79b9f53f1987ffe98d379e
Brad-- <brad.smith.1324@gmail.com>
2020-08-01 13:01:38 -0600

    get tabs in current window

--- background.js
+++ background.js
@@ -1,7 +1,15 @@
+function getTabsInActiveWindow () {
+    var queryInfo = {
+        currentWindow: true
+    }
+    chrome.tabs.query(queryInfo, saveTabList);
+}
+
+function saveTabList (tabList) {
+    console.log(tabList);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     var button = document.getElementById('archive-button');
-
-    button.addEventListener('click', (e) => {
-        console.log(chrome.tabs);
-    });
-})
+    button.addEventListener('click', getTabsInActiveWindow);
+});
