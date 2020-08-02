b197606649df48714cf1847f26c2d7803568c3dc
Brad-- <brad.smith.1324@gmail.com>
2020-08-01 20:04:40 -0600

    don't open empty default window

--- archive.js
+++ archive.js
@@ -122,19 +122,24 @@ function addArchiveToDom (archive) {
 }
 
 function createTabsInNewWindow (archiveId) {
-    var windowOptions = {
-        focused: true
-    };
-    chrome.windows.create(windowOptions, (window) => {
-        createTabs(window.id, archiveId);
-    });
-}
-
-function createTabs (windowId, archiveId) {
+    // Get the URLs, using the first to launch the window, then remove it from the list
     var archive = getArchiveById(archiveId);
     var urls = archive.tabs.map((tab) => {
         return tab.url;
     });
+    var firstUrl = urls[0];
+    urls.splice(0, 1);
+
+    var windowOptions = {
+        url: firstUrl,
+        focused: true
+    };
+    chrome.windows.create(windowOptions, (window) => {
+        createTabs(window.id, urls);
+    });
+}
+
+function createTabs (windowId, urls) {
     urls.forEach((url) => {
         var tabOptions = {
             url: url,
